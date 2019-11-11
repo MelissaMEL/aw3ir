bfa6fcfebaf70555d1576e280909c8459befe12c
MelissaMEL <52115331+MelissaMEL@users.noreply.github.com>
2019-11-11 16:10:48 +0100

    tp2

---
+++ tp2/Formulaire.js
@@ -0,0 +1,22 @@
+
+function validation() {
+    var error = "";
+    var champsList = document.querySelectorAll("#champ");
+     
+    champsList.forEach(function (champItem) {
+        if (champItem.value.length < 5) {
+            error += "champ vide : " + document.querySelector("[for='"+champItem.id+"']").textContent + "<br/>";
+        }
+        
+    };
+
+    document.getElementById("error").innerHTML = error;
+    document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value;
+  
+
+
+    console.log(error);
+    console.log(resultat);
+    return false;
+
+}
